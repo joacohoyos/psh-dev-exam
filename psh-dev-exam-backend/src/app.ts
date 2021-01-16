89dd436726762778955a5dfca10145b5ad310873
Joaquin Hoyos <joaco.hoyos@hotmail.com>
2021-01-16 15:25:45 -0300

    add headers to express

--- psh-dev-exam-backend/src/app.ts
+++ psh-dev-exam-backend/src/app.ts
@@ -6,6 +6,11 @@ import cron from './cron'
 
 const app = express()
 app.use(express.json())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(playersRouter)
 
 app.listen(PORT, () => {
